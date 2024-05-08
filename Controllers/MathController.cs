5b02af36189131a2992b65d4699fa3a23650f1f4
PC03 <PC03@INFO6-2863>
2024-05-08 08:15:02 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/MathController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_LeardiniGioele_WebAPI.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : Controller
+    {
+
+        [HttpGet("GetFibonacci")]
+        public JsonResult GetMultiplointero(int n1)
+        {
+            if (n1 <= 1)
+                return Json(new { output = n1, status = "OK" });
+            else
+                return Json(new { output = (n1 - 1) + (n1 - 2), status = "OK" });
+
+        }
+
+        [HttpGet("GetFibonacci")]
+        public JsonResult GetFibonacci(int n1)
+        {
+            if (n1 <= 1)
+                return Json(new { output = n1, status = "OK" });
+            else
+                return Json(new { output = (n1 - 1) + (n1 - 2), status = "OK" });
+
+        }
+
+
+
+    }
+}
